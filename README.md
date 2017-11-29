bf6382d4d59c3ca1be36fdcaad48c21b75f7e581
chao-samu <chao-samu@users.noreply.github.com>
2017-11-29 15:16:35 +0100

    - added responsive Size

--- README.md
+++ README.md
@@ -1,28 +1,36 @@
-# RainmeterSkin: SearchBar Everything + TC
-This is a small little Rainmeter search bar skin for the program "Everything" and "Total Commander"
-to make fast searches on specified drives and folder or files directly from the desktop.
+﻿# RainmeterSkin: SearchBar Everything + TC
+This is a small little Rainmeter search bar skin for the program "Everything"
+and "Total Commander" to make fast searches on specified drives and folder or
+files directly from the desktop.
 
 ![](@Resources/examples/skin_example.png)
 
 The Skin is inspired from "[Search+](http://bgd69.deviantart.com/art/Search-425781470)" skin by Bgd69.
 
-Special thanks goes to Christian Gishler for the permission to use his "Total Commander" Icon and
-David Carpenter to use his "Everything" Icon for this Skin.
+Special thanks goes to Christian Gishler for the permission to use his
+"Total Commander" Icon and David Carpenter to use his "Everything" icon for this
+Skin.
 
 # What makes this search bar different from others?
-1. Search bars normally don't fit in your current skin setting and it makes it quite difficult to
- adjust the skin image with your image editing software. This search bar skin is different because
- it don't uses even one image and you can change the appearance very easily with just 2 variables
- to fit it to your settings.
-2. If you make a selection in other search bars the selection will be dropped if you redraw your
- skin and it will automatically select the initial standard setting. I made this skin able to save
- it's current setting if you wish and not to save it's current setting if you don't wish. Besides
- you can also set a standard setting that will always be set if the skin is redrawn.
+1. Search bars normally don't fit in your current skin setting and it makes it
+ quite difficult to adjust the skin image with your image editing software. This
+ search bar skin is different because it don't uses even one image and you can
+ change the appearance very easily with just 2 variables to fit it to your
+ settings.
+2. If you make a selection in other search bars the selection will be dropped if
+ you redraw your skin and it will automatically select the initial standard
+ setting. I made this skin able to save it's current setting if you wish and not
+ to save it's current setting if you don't wish. Besides you can also set a
+ standard setting that will always be set if the skin is redrawn.
+3. Easily adjustable for high dpi screens.
 
 # How to use?
 1. It's a Rainmeter skin you need Rainmeter
-2. You need "[Everything](http://bgd69.deviantart.com/art/Search-425781470)" installed and/or "[Total Commander](https://www.ghisler.com)"
+2. You need "[Everything](https://www.voidtools.com/)"
+   installed and/or "[Total Commander](https://www.ghisler.com)"
 3. Install the *.rmskin file
-4. Important! Edit the "SearchBar.cfg" to your needs. Add the drives you want and the correct
- program paths. Keep it LUA language conform (normally you just need to know that you have to
- use double backslashes instead of single backslashes)
+4. Important! Edit the "SearchBar.cfg" to your needs. Add the drives you want
+   and the correct program paths. Keep it LUA language conform (normally you
+   just need to know that you have to use double backslashes instead of single
+   backslashes)
+5. If you wish, change appearance (Color, Size...) in the Skin ini file.
--- SearchMode.lua
+++ SearchMode.lua
@@ -1,17 +1,21 @@
 function Initialize()
+    -- DEVELOPER NOTES =========================================================
 
-    -- VARIABLES ===================================================================================
+    --[[
+    ]]--
+
+    -- INCLUDE =================================================================
 
     -- insert cfg file variables
     dofile(SKIN:MakePathAbsolute('SearchBar.cfg'))
 
-    -- PREPARE VARIABLES ===========================================================================
+    -- PREPARE VARIABLES =======================================================
 
     -- get array size
     maxIndexDrive = iterate(drive)
     maxIndexSScope = iterate(sScope)
 
-    --Everything drive letters
+    -- "Everything" drive letters
     driveEverything = {}
     driveEverything[1] = ""
     for t, _ in ipairs(drive) do
@@ -20,7 +24,7 @@ function Initialize()
         end
     end
 
-    --Total Commander drive letters
+    -- "Total Commander" drive letters
     driveTC = {}
     driveTC[1] = ""
     for t, _ in ipairs(drive) do
@@ -34,31 +38,166 @@ function Initialize()
         end
     end
 
-   -- INIT SKIN SETTINGS ===========================================================================
+    -- SET SKIN SIZE WITH "SIZE" VARIABLE ======================================
 
-    if count % 2 == 0 then
-        SKIN:Bang('!SetOption ProgrammICON ImageName "' .. ImageNameEverything .. '"')
-        SKIN:Bang('!SetOption SearchBarText Text "Everything"')
+    -- get size
+    SearchBar_Size = SKIN:GetVariable('Size')
+    SearchBar_Size = SearchBar_Size/100
+
+    -- set Variables (SearchBar_MyModifiers1)
+    local SearchBar_MyModifiers1 = SKIN:GetVariable('MyModifiers1')
+
+    SearchBar_MyModifiers1_StrokeWidth = string.match(SearchBar_MyModifiers1, '.-StrokeWidth%s*(%d+)')
+    SearchBar_MyModifiers1_StrokeWidth = SearchBar_MyModifiers1_StrokeWidth * SearchBar_Size
+    SearchBar_MyModifiers1 = string.gsub(SearchBar_MyModifiers1, 'StrokeWidth.-|' ,
+    'StrokeWidth ' .. SearchBar_MyModifiers1_StrokeWidth .. ' |', 1)
+
+    SKIN:Bang('!SetVariable "MyModifiers1" "' .. SearchBar_MyModifiers1 .. '"')
+
+    -- set Plugin_InputText
+    local Plugin_InputText = SKIN:GetMeasure('Plugin_InputText')
+    Plugin_InputText_X = Plugin_InputText:GetNumberOption('X')
+    Plugin_InputText_Y = Plugin_InputText:GetNumberOption('Y')
+    Plugin_InputText_W = Plugin_InputText:GetNumberOption('W')
+    Plugin_InputText_H = Plugin_InputText:GetNumberOption('H')
+    Plugin_InputText_FontSize = Plugin_InputText:GetNumberOption('FontSize')
+    -- Note MEASURE "Input Plugin" cannot handle even pixel inch (tested in rainmeter v4.0)
+    Plugin_InputText_X = math.floor((Plugin_InputText_X * SearchBar_Size)+0.5)
+    Plugin_InputText_Y = math.floor((Plugin_InputText_Y * SearchBar_Size)+0.5)
+    Plugin_InputText_W = math.floor((Plugin_InputText_W * SearchBar_Size)+0.5)
+    Plugin_InputText_H = math.floor((Plugin_InputText_H * SearchBar_Size)+0.5)
+    Plugin_InputText_FontSize = Plugin_InputText_FontSize * SearchBar_Size
+
+    SKIN:Bang('!SetOption "Plugin_InputText" "X" " ' .. Plugin_InputText_X .. '"')
+    SKIN:Bang('!SetOption "Plugin_InputText" "Y" " ' .. Plugin_InputText_Y .. '"')
+    SKIN:Bang('!SetOption "Plugin_InputText" "W" " ' .. Plugin_InputText_W .. '"')
+    SKIN:Bang('!SetOption "Plugin_InputText" "H" " ' .. Plugin_InputText_H .. '"')
+    SKIN:Bang('!SetOption "Plugin_InputText" "FontSize" " ' .. Plugin_InputText_FontSize .. '"')
+
+    -- set Background_SearchMode
+    l_object_Position = GetMeterPosition('Background_SearchMode')
+    l_object_Size = GetMeterShapeSize('Background_SearchMode')
+    SetSizetoObject(l_object_Position)
+    SetSizetoObject(l_object_Size)
+    SetMeterPosition('Background_SearchMode', l_object_Position)
+    SetMeterShapeSize('Background_SearchMode', l_object_Size)
+    -- set Background_Drive
+    l_object_Position = GetMeterPosition('Background_Drive')
+    l_object_Size = GetMeterShapeSize('Background_Drive')
+    SetSizetoObject(l_object_Position)
+    SetSizetoObject(l_object_Size)
+    SetMeterPosition('Background_Drive', l_object_Position)
+    SetMeterShapeSize('Background_Drive', l_object_Size)
+    -- set Background_SearchScope
+    l_object_Position = GetMeterPosition('Background_SearchScope')
+    l_object_Size = GetMeterShapeSize('Background_SearchScope')
+    SetSizetoObject(l_object_Position)
+    SetSizetoObject(l_object_Size)
+    SetMeterPosition('Background_SearchScope', l_object_Position)
+    SetMeterShapeSize('Background_SearchScope', l_object_Size)
+    -- set Background_SearchBar
+    l_object_Position = GetMeterPosition('Background_SearchBar')
+    l_object_Size = GetMeterShapeSize('Background_SearchBar')
+    SetSizetoObject(l_object_Position)
+    SetSizetoObject(l_object_Size)
+    SetMeterPosition('Background_SearchBar', l_object_Position)
+    SetMeterShapeSize('Background_SearchBar', l_object_Size)
+
+    -- set Icon_SearchMode
+    local Icon_SearchMode = SKIN:GetMeter('Icon_SearchMode')
+    Icon_SearchMode_X = Icon_SearchMode:GetX()
+    Icon_SearchMode_Y = Icon_SearchMode:GetY()
+    Icon_SearchMode_W = Icon_SearchMode:GetW()
+    Icon_SearchMode_H = Icon_SearchMode:GetH()
+
+    Icon_SearchMode_X = Icon_SearchMode_X * SearchBar_Size
+    Icon_SearchMode_Y = Icon_SearchMode_Y * SearchBar_Size
+    Icon_SearchMode_W = Icon_SearchMode_W * SearchBar_Size
+    Icon_SearchMode_H = Icon_SearchMode_H * SearchBar_Size
+
+    SKIN:Bang('!SetOption "Icon_SearchMode" "X" " ' .. Icon_SearchMode_X .. '"')
+    SKIN:Bang('!SetOption "Icon_SearchMode" "Y" " ' .. Icon_SearchMode_Y .. '"')
+    SKIN:Bang('!SetOption "Icon_SearchMode" "W" " ' .. Icon_SearchMode_W .. '"')
+    SKIN:Bang('!SetOption "Icon_SearchMode" "H" " ' .. Icon_SearchMode_H .. '"')
+
+    -- set icon Size
+    if Icon_SearchMode_W <= 16 then
+        ImageNameEverything = SKIN:ReplaceVariables('#@#images\\Everything\\16x16_Everything-1.4.1.877.x64-Setup.png')
+        ImageNameTC = SKIN:ReplaceVariables('#@#images\\TC\\16x16_AppIcon6.png')
+    elseif Icon_SearchMode_W <= 24 then
+        ImageNameEverything = SKIN:ReplaceVariables('#@#images\\Everything\\24x24_Everything-1.4.1.877.x64-Setup.png')
+        ImageNameTC = SKIN:ReplaceVariables('#@#images\\TC\\24x24_AppIcon6.png')
+    elseif Icon_SearchMode_W <= 32 then
+        ImageNameEverything = SKIN:ReplaceVariables('#@#images\\Everything\\32x32_Everything-1.4.1.877.x64-Setup.png')
+        ImageNameTC = SKIN:ReplaceVariables('#@#images\\TC\\32x32_AppIcon6.png')
+    elseif Icon_SearchMode_W <= 48 then
+        ImageNameEverything = SKIN:ReplaceVariables('#@#images\\Everything\\48x48_Everything-1.4.1.877.x64-Setup.png')
+        ImageNameTC = SKIN:ReplaceVariables('#@#images\\TC\\48x48_AppIcon6.png')
+    else  --Icon_SearchMode_W > 48
+        ImageNameEverything = SKIN:ReplaceVariables('#@#images\\Everything\\256x256_Everything-1.4.1.877.x64-Setup.png')
+        ImageNameTC = SKIN:ReplaceVariables('#@#images\\TC\\48x48_AppIcon6.png')
+    end
+
+    -- set Text_Drive
+    l_object_Position = GetMeterPosition('Text_Drive')
+    l_object_Size = GetMeterSize('Text_Drive')
+    l_FontSize = GetFontSize('Text_Drive')
+    SetSizetoObject(l_object_Position)
+    SetSizetoObject(l_object_Size)
+    SetSizetoObject(l_FontSize)
+    SetMeterPosition('Text_Drive', l_object_Position)
+    SetMeterSize('Text_Drive', l_object_Size)
+    SetFontSize('Text_Drive', l_FontSize)
+
+    -- set Text_SearchScope
+    l_object_Position = GetMeterPosition('Text_SearchScope')
+    l_object_Size = GetMeterSize('Text_SearchScope')
+    l_FontSize = GetFontSize('Text_SearchScope')
+    SetSizetoObject(l_object_Position)
+    SetSizetoObject(l_object_Size)
+    SetSizetoObject(l_FontSize)
+    SetMeterPosition('Text_SearchScope', l_object_Position)
+    SetMeterSize('Text_SearchScope', l_object_Size)
+    SetFontSize('Text_SearchScope', l_FontSize)
+
+    -- set Text_SearchBar
+    l_object_Position = GetMeterPosition('Text_SearchBar')
+    l_object_Size = GetMeterSize('Text_SearchBar')
+    l_FontSize = GetFontSize('Text_SearchBar')
+    SetSizetoObject(l_object_Position)
+    SetSizetoObject(l_object_Size)
+    SetSizetoObject(l_FontSize)
+    SetMeterPosition('Text_SearchBar', l_object_Position)
+    SetMeterSize('Text_SearchBar', l_object_Size)
+    SetFontSize('Text_SearchBar', l_FontSize)
+
+   -- INIT SKIN SETTINGS =======================================================
+
+    if count == 1 then
+        SKIN:Bang('!SetOption Icon_SearchMode ImageName "' .. ImageNameTC .. '"')
+        SKIN:Bang('!SetOption Text_SearchBar Text "Total Commander"')
     else
-        SKIN:Bang('!SetOption ProgrammICON ImageName "' .. ImageNameTC .. '"')
-        SKIN:Bang('!SetOption SearchBarText Text "Total Commander"')
+        count = 0
+        SKIN:Bang('!SetOption Icon_SearchMode ImageName "' .. ImageNameEverything .. '"')
+        SKIN:Bang('!SetOption Text_SearchBar Text "Everything"')
     end
 
     if count2 <= maxIndexDrive then
-        SKIN:Bang('!SetOption DriveICON Text "' .. drive[count2] .. '"')
+        SKIN:Bang('!SetOption Text_Drive Text "' .. drive[count2] .. '"')
     else
         count2 = 1
-        SKIN:Bang('!SetOption DriveICON Text "' .. drive[count2] .. '"')
+        SKIN:Bang('!SetOption Text_Drive Text "' .. drive[count2] .. '"')
     end
 
     if count3 <= maxIndexSScope then
-        SKIN:Bang('!SetOption SearchScopeICON Text "' .. sScope[count3] .. '"')
+        SKIN:Bang('!SetOption Text_SearchScope Text "' .. sScope[count3] .. '"')
     else
         count3 = 1
-        SKIN:Bang('!SetOption SearchScopeICON Text "' .. sScope[count3] .. '"')
+        SKIN:Bang('!SetOption Text_SearchScope Text "' .. sScope[count3] .. '"')
     end
 
-    SKIN:Bang('[!UpdateMeter SearchBarText][!UpdateMeter ProgrammICON][!UpdateMeter DriveICON][!UpdateMeter SearchScopeICON][!Redraw]')
+    -- Update Skin =============================================================
+    SKIN:Bang('[!Update][!Redraw]')
 
 end
 
@@ -66,11 +205,22 @@ function Update()
    -- pass
 end
 
+
+
+
+
+
+
+-- FUNCTIONS: user action handling =============================================
+
 function UserInput()
 
-    MyMeasure = SKIN:GetMeasure('InputText')
+    MyMeasure = SKIN:GetMeasure('Plugin_InputText')
     local UserInput = MyMeasure:GetStringValue()
-    if count % 2 == 0 then
+    if count == 1 then
+        SKIN:Bang('["' .. PathTCScript .. '" "' .. PathTC .. '" "' .. sScopeTC[count3] .. '" "'.. driveTC[count2] .. '" "' .. UserInput .. '"]')
+        SKIN:Bang('!SetOption Text_SearchBar Text "Total Commander"')
+    else
         if count2 == 1 or count3 == 1 then
             if count2 == 1 and count3 == 1 then
             SKIN:Bang('["' .. PathEverything .. '" -s "' .. UserInput .. '"]')
@@ -82,86 +232,84 @@ function UserInput()
         else
         SKIN:Bang('["' .. PathEverything .. '" -s "' .. sScopeEverything[count3] .. ' '.. driveEverything[count2] .. ' ' .. UserInput .. '"]')
         end
-        SKIN:Bang('!SetOption SearchBarText Text "Everything"')
-    else
-        SKIN:Bang('["' .. PathTCScript .. '" "' .. PathTC .. '" "' .. sScopeTC[count3] .. '" "'.. driveTC[count2] .. '" "' .. UserInput .. '"]')
-        SKIN:Bang('!SetOption SearchBarText Text "Total Commander"')
+        SKIN:Bang('!SetOption Text_SearchBar Text "Everything"')
     end
 
-    SKIN:Bang('[!UpdateMeter SearchBarText][!Redraw]')
+    SKIN:Bang('[!UpdateMeter Text_SearchBar][!Redraw]')
 
 end
 
-function ProgrammICON()
+-- ToDo: fix range
+function Icon_SearchMode()
 
     count = count + 1
 
-    if count % 2 == 0 then
-        SKIN:Bang('!SetOption ProgrammICON ImageName "' .. ImageNameEverything .. '"')
-        SKIN:Bang('!SetOption SearchBarText Text "Everything"')
+    if count == 1 then
+        SKIN:Bang('!SetOption Icon_SearchMode ImageName "' .. ImageNameTC .. '"')
+        SKIN:Bang('!SetOption Text_SearchBar Text "Total Commander"')
     else
-        count = 1
-        SKIN:Bang('!SetOption ProgrammICON ImageName "' .. ImageNameTC .. '"')
-        SKIN:Bang('!SetOption SearchBarText Text "Total Commander"')
+        count = 0
+        SKIN:Bang('!SetOption Icon_SearchMode ImageName "' .. ImageNameEverything .. '"')
+        SKIN:Bang('!SetOption Text_SearchBar Text "Everything"')
     end
 
-    SKIN:Bang('[!UpdateMeter SearchBarText][!UpdateMeter ProgrammICON][!Redraw]')
+    SKIN:Bang('[!UpdateMeter Text_SearchBar][!UpdateMeter Icon_SearchMode][!Redraw]')
 
     saveLUAConfigVALUE()
 
 end
 
-function DriveICON()
+function Text_Drive()
 
     count2 = count2 + 1
 
     if count2 <= maxIndexDrive then
-        SKIN:Bang('!SetOption DriveICON Text "' .. drive[count2] .. '"')
+        SKIN:Bang('!SetOption Text_Drive Text "' .. drive[count2] .. '"')
     else
         count2 = 1
-        SKIN:Bang('!SetOption DriveICON Text "' .. drive[count2] .. '"')
+        SKIN:Bang('!SetOption Text_Drive Text "' .. drive[count2] .. '"')
     end
 
-    SKIN:Bang('[!UpdateMeter DriveICON][!Redraw]')
+    SKIN:Bang('[!UpdateMeter Text_Drive][!Redraw]')
 
     saveLUAConfigVALUE()
 
 end
 
-function SearchScopeICON()
+function Text_SearchScope()
 
     count3 = count3 + 1
 
     if count3 <= maxIndexSScope then
-        SKIN:Bang('!SetOption SearchScopeICON Text "' .. sScope[count3] .. '"')
+        SKIN:Bang('!SetOption Text_SearchScope Text "' .. sScope[count3] .. '"')
     else
         count3 = 1
-        SKIN:Bang('!SetOption SearchScopeICON Text "' .. sScope[count3] .. '"')
+        SKIN:Bang('!SetOption Text_SearchScope Text "' .. sScope[count3] .. '"')
     end
 
-    SKIN:Bang('[!UpdateMeter SearchScopeICON][!Redraw]')
+    SKIN:Bang('[!UpdateMeter Text_SearchScope][!Redraw]')
 
     saveLUAConfigVALUE()
 
 end
 
-function SearchBarTextRestore()
+function Text_SearchBar_Restore()
 
-    if count % 2 == 0 then
-        SKIN:Bang('!SetOption SearchBarText Text "Everything"')
+    if count == 1 then
+        SKIN:Bang('!SetOption Text_SearchBar Text "Total Commander"')
     else
-        SKIN:Bang('!SetOption SearchBarText Text "Total Commander"')
+        SKIN:Bang('!SetOption Text_SearchBar Text "Everything"')
     end
 
-    SKIN:Bang('[!UpdateMeter SearchBarText][!Redraw]')
+    SKIN:Bang('[!UpdateMeter Text_SearchBar][!Redraw]')
 
 end
 
-function iterate(drive)
+function iterate(d_Object)
 
     local index = 0
 
-    for _ in pairs(drive) do
+    for _ in pairs(d_Object) do
         index = index + 1
     end
 
@@ -186,5 +334,93 @@ function saveLUAConfigVALUE()
 
     fp:write(content)
     fp:close()
+
+end
+
+-- FUNCTIONS: Size handling ====================================================
+
+-- get Functions
+function GetMeterPosition(MeterName)
+
+    local MeterObject = SKIN:GetMeter(MeterName)
+    local MeterObject_X = MeterObject:GetX()
+    local MeterObject_Y = MeterObject:GetY()
+
+    return { ['X'] = MeterObject_X, ['Y'] = MeterObject_Y}
+
+end
+
+function GetMeterSize(MeterName)
+
+    local MeterObject = SKIN:GetMeter(MeterName)
+    local MeterObject_W = MeterObject:GetW()
+    local MeterObject_H = MeterObject:GetH()
+
+    return {['W'] = MeterObject_W, ['H'] = MeterObject_H}
+
+end
+
+function GetMeterShapeSize(MeterName)
+
+    local MeterObject = SKIN:GetMeter(MeterName)
+    MeterObject_Shape = MeterObject:GetOption('Shape')
+    local MeterObject_Shape_X = string.match(MeterObject_Shape, '%a+%s+(%d+),%d+,%d+,%d+')
+    local MeterObject_Shape_Y = string.match(MeterObject_Shape, '%a+%s+%d+,(%d+),%d+,%d+')
+    local MeterObject_Shape_W = string.match(MeterObject_Shape, '%a+%s+%d+,%d+,(%d+),%d+')
+    local MeterObject_Shape_H = string.match(MeterObject_Shape, '%a+%s+%d+,%d+,%d+,(%d+)')
+
+    return {['X'] = MeterObject_Shape_X, ['Y'] = MeterObject_Shape_Y, ['W'] = MeterObject_Shape_W, ['H'] = MeterObject_Shape_H}
+
+end
+
+function GetFontSize(MeterName)
+
+    local MeterObject = SKIN:GetMeter(MeterName)
+    MeterObject_FontSize = MeterObject:GetOption('InlineSetting2')
+    MeterObject_FontSize = string.match(MeterObject_FontSize, 'Size%s*|%s*(%d+)')
+
+    return {['FontSize'] = tonumber(MeterObject_FontSize)}
+
+end
+
+-- set Functions (global)
+function SetSizetoObject(t_Object)
+    -- needs global value "SearchBar_Size"
+    for k, v in pairs(t_Object) do
+        t_Object[k] = v * SearchBar_Size
+    end
+
+end
+
+-- set Functions (local)
+function SetMeterPosition(MeterName, t_MeterSize)
+
+    SKIN:Bang('!SetOption "' .. MeterName .. '" "X" " ' .. t_MeterSize['X'] .. '"')
+    SKIN:Bang('!SetOption "' .. MeterName .. '" "Y" " ' .. t_MeterSize['Y'] .. '"')
+
+end
+
+function SetMeterSize(MeterName, t_MeterSize)
+
+    SKIN:Bang('!SetOption "' .. MeterName .. '" "W" " ' .. t_MeterSize['W'] .. '"')
+    SKIN:Bang('!SetOption "' .. MeterName .. '" "H" " ' .. t_MeterSize['H'] .. '"')
+
+end
+
+function SetMeterShapeSize(MeterName, t_MeterShapeSize)
+
+    MeterObject_Shape = string.gsub(MeterObject_Shape, '.-%s|' ,
+    'Rectangle ' .. t_MeterShapeSize['X'] .. ',' .. t_MeterShapeSize['Y'] ..
+    ',' .. t_MeterShapeSize['W'] .. ',' .. t_MeterShapeSize['H'] .. ' |', 1)
+
+    SKIN:Bang('!SetOption "' .. MeterName .. '" "Shape" "' .. MeterObject_Shape .. '"')
+
+end
+
+function SetFontSize(MeterName, t_FontSize)
+
+    MeterObject_FontSize = string.gsub(MeterObject_FontSize, '.*', 'Size | ' .. t_FontSize['FontSize'], 1)
+
+    SKIN:Bang('!SetOption "' .. MeterName .. '" "InlineSetting2" " ' .. MeterObject_FontSize .. '"')
 
 end
